ba8d408b5e76c16c9487bed21d1d2b660ef0d231
Erick <edeajob27@gmail.com>
2023-09-18 15:01:47 -0500

    Rename a movies.py a data.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, Body, HTTPException
+from typing import List
+
+from fastapi import FastAPI, Body, HTTPException, Path, Query
 from fastapi.responses import HTMLResponse
 
-from models import Movie
-from movies import movies_list
+from data import movies_list
+from models import Movie, MovieCategory
 
 app = FastAPI(title='My Movie App', version='0.0.1', description='Una api de introducción a FastAPI')
 
@@ -12,30 +14,29 @@ def root():
     return HTMLResponse('<h1>Hola soy un html response</h1>')
 
 
-@app.get("/movies", tags=['movies'])
-def get_movies():
+@app.get("/movies", tags=['movies'], response_model=List[Movie])
+def get_movies() -> List[Movie]:
     return movies_list
 
 
-@app.get("/movies/{id}", tags=['movies'])
-def get_movie_by_id(movie_id: int):
+@app.get("/movies/{id}", tags=['movies'], response_model=Movie)
+def get_movie_by_id(movie_id: int = Path(ge=1, le=100)) -> Movie:
     for item in movies_list:
         if item['id'] == movie_id:
             return item
     raise HTTPException(status_code=404, detail=f'No se encontró la película con el id {movie_id}')
 
 
-@app.get("/movies/", tags=['movies'])
-def get_movies_by_category(category: str, year: int):
-    filtered_movies = [movie for movie in movies_list if movie['category'] == category and movie['year'] == year]
+@app.get("/movies/", tags=['movies'], response_model=List[Movie])
+def get_movies_by_category(category: MovieCategory = Query(min_length=5, max_length=15)) -> List[Movie]:
+    filtered_movies = [movie for movie in movies_list if movie['category'] == category]
     if filtered_movies:
         return filtered_movies
-    raise HTTPException(status_code=404, detail=f'No hay películas para la categoría {category} y el año {year}')
+    raise HTTPException(status_code=404, detail=f'No hay películas para la categoría {category}')
 
 
 @app.post("/movies", tags=['movies'])
 def create_movie(movie: Movie = Body(...)):
-
     # Verificar que no existe una película con el mismo id
     if any(existing_movie['id'] == movie.id for existing_movie in movies_list):
         raise HTTPException(status_code=400, detail=f"Ya existe una película con el ID {movie.id}.")
--- models.py
+++ models.py
@@ -1,13 +1,23 @@
+from enum import Enum
+
 from pydantic import BaseModel, Field
+
+
+class MovieCategory(str, Enum):
+    action = "Acción"
+    drama = "Drama"
+    comedia = "Comedia"
+    fiction = "Ciencia Ficción"
+    other = "Otro"
 
 
 class Movie(BaseModel):
     id: int
     title: str = Field(min_length=5, max_length=20)
-    overview: str = Field(min_length=5, max_length=30)
+    overview: str = Field(min_length=5, max_length=100)
     year: int = Field(le=2023, ge=1900)
     rating: float = Field(ge=0, le=10.0)
-    category: str = Field(max_length=15)
+    category: MovieCategory = Field(max_length=15)
 
     class Config:
         json_schema_extra = {
