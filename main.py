aa65d834260cff29ce43e6c2c97f564dc8caf43d
Erick <edeajob27@gmail.com>
2023-10-03 07:28:22 -0500

    creacion de un router para los usuarios

--- main.py
+++ main.py
@@ -3,23 +3,16 @@ from fastapi.responses import HTMLResponse
 
 from config.database import Base, engine
 from middelwares.error_handler import ErrorHandler
-from middelwares.jwt_manager import create_token
-from models.models import User
 from routers.movie import movie_router
+from routers.users_router import user_router
 
 app = FastAPI(title='My Movie App', version='0.0.1', description='Una api de introducción a FastAPI')
 Base.metadata.create_all(bind=engine)
 app.add_middleware(ErrorHandler)
+app.include_router(user_router)
 app.include_router(movie_router)
 
 
 @app.get("/", tags=['home'], status_code=200)
 def root():
     return HTMLResponse('<h1>Hola soy un html response</h1>')
-
-
-@app.post('/login', tags=['auth'], status_code=200)
-def login(user: User):
-    if user.email == "admin@gmail.com" and user.password == "admin":
-        token: str = create_token(user.model_dump())
-        return token
---
+++ routers/users_router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from middelwares.jwt_manager import create_token
+from models.models import User
+
+user_router = APIRouter()
+
+
+@user_router.post('/login', tags=['auth'], status_code=200)
+def login(user: User):
+    if user.email == "admin@gmail.com" and user.password == "admin":
+        token: str = create_token(user.model_dump())
+        return token
